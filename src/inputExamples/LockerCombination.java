b1324357d472e8428ebd3eefe30aa5f29f49fba6
Steve Schmid <steveschmid314@gmail.com>
2018-09-21 12:55:17 -0500

    Locker Combination Program

--- src/inputExamples/LockerCombination.java
+++ src/inputExamples/LockerCombination.java
@@ -1,5 +1,28 @@
 package inputExamples;
 
-public class LockerCombination {
+import java.util.Scanner;
 
+public class LockerCombination 
+{
+	public static void main(String[] args)
+	{
+		//initialize variables
+		Scanner input = new Scanner(System.in);
+		int num1 = 0;
+		int num2 = 0;
+		int num3 = 0;
+		
+		//receive input
+		System.out.println("Locker Combination");
+		System.out.println("Number 1:");
+		num1 = input.nextInt();
+		System.out.println("Number 2:");
+		num2 = input.nextInt();
+		System.out.println("Number 3:");
+		num3 = input.nextInt();
+		
+		//output
+		System.out.println("Your locker combination is \n" + num1 + " - " + num2 + " - " + num3);
+		input.close();
+	}
 }
